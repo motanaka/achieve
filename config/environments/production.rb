06bb8e5288cd12a3bb242340e8647ff361576e88
Motoo TANAKA <motanaka@nttpc.co.jp>
2017-05-11 07:55:22 +0000

    retry mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,9 +77,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { host:calm-bastion-94107.herokuapp.com }
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings =
+   config.action_mailer.default_url_options = { host:'calm-bastion-94107.herokuapp.com' }
+   ActionMailer::Base.delivery_method = :smtp
+   ActionMailer::Base.smtp_settings =
    {
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
