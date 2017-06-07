e0de6a30388242f19dd0238a7064982dc782baac
Motoo TANAKA <motanaka@nttpc.co.jp>
2017-06-07 09:36:12 +0000

    follow_05

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,10 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
-    @follower = Relationship.find(params[:id]).follower
-    @follower.save
-    @followed = Relationship.find(params[:id]).followed
-    @followed.save
+#    @follower = Relationship.find(params[:id]).follower
+#    @follower.save
+#    @followed = Relationship.find(params[:id]).followed
+#    @followed.save
   end
   
 end
