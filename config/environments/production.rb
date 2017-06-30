c801bbb328878b1c0c5ab83b2bb0e2201299ef31
Motoo TANAKA <motanaka@nttpc.co.jp>
2017-06-30 05:19:48 +0000

    aws deploy mail1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-   config.action_mailer.default_url_options = { host: 'calm-bastion-94107.herokuapp.com' }
+   config.action_mailer.default_url_options = { host: '52.69.150.80' }
    ActionMailer::Base.delivery_method = :smtp
    ActionMailer::Base.smtp_settings =
    {
